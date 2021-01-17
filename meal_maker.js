d7d78592d2c1397588380b463f9d6384029deede
Joshua Rios <1984yearoftherat@gmail.com>
2021-01-17 09:59:49 -1000

    finished product

--- meal_maker.js
+++ meal_maker.js
@@ -1,53 +1,95 @@
+/*******************************Begin menu object************************************ */
 const menu = {
+
+  // defined _courses object with three keys; appetizers, mains, and desserts and all their values are set to empty arrays.
   _courses: {
     appetizers: [],
     mains: [],
-    desserts: [],
+    desserts: []
   },
+
+  // getter method that returns the value in this._courses.appetizers which should be an array
   get appetizers() {
     return this._courses.appetizers;
   },
-  set appetizers(appetizerIn) {
-    this._courses.appetizers.push(appetizerIn);
-  },
+
+  // getter method that returns the value in this._courses.mains which should be an array
   get mains() {
     return this._courses.mains;
   },
-  set mains(mainIn) {
-    this._courses.mains.push(mainIn);
-  },
+
+  // getter method that returns the value in this._courses.desserts which should be an array
   get desserts() {
     return this._courses.desserts;
   },
+
+   // getter method that returns the value in this._courses which should be an object that returns an object with three _courses keys; appetizers, mains, and desserts.
+   get courses() {
+    return this._courses;
+  },
+
+  // setter method that takes an object as parameter and pushes it into this._courses.appetizers array
+  set appetizers(appetizerIn) {
+    this._courses.appetizers.push(appetizerIn);
+  },
+
+  // setter method that takes an object as parameter and pushes it into this._courses.mains array
+  set mains(mainIn) {
+    this._courses.mains.push(mainIn);
+  },
+
+  // setter method that takes an object as parameter and pushes it into this._courses.desserts array
   set desserts(dessertIn) {
     this._courses.desserts.push(dessertIn);
   },
-  get courses() {
-    return {
-      appetizers: this.appetizers,
-      mains: this.mains,
-      desserts: this.desserts,
-    };
-  },
+
+  // This uses the setter methods to add a dish with a price to the appropriate course item
   addDishToCourse(courseName, dishName, dishPrice) {
-    this[courseName] = { [dishName]: dishPrice };
+    this[courseName] = {[dishName]: dishPrice};
   },
+
+  // This method is supposed to find the price of the dish object
+  findPriceOfDish(dish) {
+    const dishPrice = Object.values(dish);
+    return Number(dishPrice);
+  },
+
+  // This generates a random index number between 0 and the length of the dishes array rounded down so -1. Returns the dishes array element at the random index.
   getRandomDishFromCourse(courseName) {
-    const dishes = this[courseName];
+    const dishes = this._courses[courseName];
     const randIndex = Math.floor(Math.random() * dishes.length);
     return dishes[randIndex];
   },
+
+  // This method generates a random 3 course meal with the prices. Returns an interpolated string.
   generateRandomMeal() {
-    const appetizer = this.getRandomDishFromCourse("appetizers");
+
+    // Saved each random course to its appropriate variable
+    const appetizer = this.getRandomDishFromCourse('appetizers');
     const main = this.getRandomDishFromCourse("mains");
     const dessert = this.getRandomDishFromCourse("desserts");
-    const price = Object.values(appetizer)[0] +
-        Object.values(main)[0] +
-        Object.values(dessert)[0];
-    return `Welcome to the Steak-Out Steak-House. May I recommend you start off with ${Object.keys(appetizer)[0]} for $${Object.values(appetizer)[0]} followed by hearty ${Object.keys(main)[0]} for $${Object.values(main)[0]} and for dessert may I suggest ${Object.keys(dessert)[0]} for $${Object.values(dessert)[0]}. The total of this meal will be $${price.toFixed(2)} plus tip of course.`;
+
+    // Used Obeject.keys() at index [0] for each varaiable above and saved it to 3 variables which should be the name of the dish.
+    const appetizerName = Object.keys(appetizer)[0];
+    const mainName = Object.keys(main)[0];
+    const dessertName = Object.keys(dessert)[0];
+    
+    // Used the findPriceOfDish method for each course and saved it appropriate variables which should be the price of the dish
+    const appetizerPrice = this.findPriceOfDish(appetizer);
+    const mainPrice = this.findPriceOfDish(main);
+    const dessertPrice = this.findPriceOfDish(dessert);
+
+    // total is all the course prices added together
+    const total = appetizerPrice + mainPrice + dessertPrice;
+
+    // This is the crown jewel: a formatted string that lists out the random courses with prices and total.
+    return `Welcome to the Steak-Out Steak-House. May I recommend you start off with ${appetizerName} for $${appetizerPrice} followed by hearty ${mainName} for $${mainPrice} and for dessert may I suggest ${dessertName} for $${dessertPrice}. The total of this meal will be $${total} plus tip of course.`;
   }
 };
+/**************************************end menu object******************************************** */
 
+
+/*****************************this section is populating the menu********************************** */
 menu.addDishToCourse("appetizers", "mini tacos", 5.99);
 menu.addDishToCourse("appetizers", "jalapeno poppers", 6.99);
 menu.addDishToCourse("appetizers", "escargo", 9.99);
@@ -67,6 +109,10 @@ menu.addDishToCourse("desserts", "a slice of our famous pies", 5.99);
 menu.addDishToCourse("desserts", "chocolate lava creme brule", 6.99);
 menu.addDishToCourse("desserts", "gummy bears", 0.99);
 menu.addDishToCourse("desserts", "m and m's", 0.99);
+menu.addDishToCourse("desserts", "slurpee", 1.99);
 
+// This section generates a random meal and logs it to the console.
 let meal = menu.generateRandomMeal();
-console.log(meal.courses);
+console.log(meal);
+
+/************************************end*************************************** */
